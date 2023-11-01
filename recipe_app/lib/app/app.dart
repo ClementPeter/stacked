03d0330631a7b56e0b8199321acc94e542c15d69
ClementPeter <clementpeter2000@gmail.com>
2023-11-01 22:59:04 +0100

    🚀stacked_app

--- recipe_app/lib/app/app.dart
+++ recipe_app/lib/app/app.dart
@@ -1,22 +1,16 @@
-import 'package:my_first_app/ui/bottom_sheets/notice/notice_sheet.dart';
-import 'package:my_first_app/ui/dialogs/info_alert/info_alert_dialog.dart';
-import 'package:my_first_app/ui/views/home/home_view.dart';
-import 'package:my_first_app/ui/views/startup/startup_view.dart';
+import 'package:recipe_app/ui/bottom_sheets/notice/notice_sheet.dart';
+import 'package:recipe_app/ui/dialogs/info_alert/info_alert_dialog.dart';
+import 'package:recipe_app/ui/views/home/home_view.dart';
+import 'package:recipe_app/ui/views/startup/startup_view.dart';
 import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
-import 'package:my_first_app/ui/views/counter/counter_view.dart';
-
-import 'package:my_first_app/ui/views/splash/splash_view.dart';
 // @stacked-import
 
 @StackedApp(
   routes: [
     MaterialRoute(page: HomeView),
     MaterialRoute(page: StartupView),
-    CustomRoute(page: CounterView),
-
-    MaterialRoute(page: SplashView),
-// @stacked-route
+    // @stacked-route
   ],
   dependencies: [
     LazySingleton(classType: BottomSheetService),
