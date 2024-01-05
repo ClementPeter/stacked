77e1b3b1ab5dc3bd9f4ec8e9be0f65cc0f32b716
ClementPeter <clementpeter2000@gmail.com>
2024-01-05 21:06:09 +0100

    🚀palette

--- my_recipe_app/lib/ui/common/app_theme.dart
+++ my_recipe_app/lib/ui/common/app_theme.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_recipe_app/extension/app_typography.dart';
+import 'package:my_recipe_app/extension/pallete.dart';
+import 'package:my_recipe_app/ui/common/app_colors.dart';
 import 'package:my_recipe_app/ui/common/app_text_styles.dart';
 
 //App theme is where we specify the global theming properties for
@@ -22,6 +24,26 @@ class AppTheme {
           labelRegular14: AppTextStyles.labelRegular14,
           labelRegular16: AppTextStyles.labelRegular16,
           labelMedium16: AppTextStyles.labelRegular16,
+        ),
+        Palette(
+          grey1: AppColors.grey1,
+          grey6: AppColors.grey6,
+          grey7: AppColors.grey7,
+          grey8: AppColors.grey8,
+          grey9: AppColors.grey9,
+          grey11: AppColors.grey11,
+          grey12: AppColors.grey12,
+          grey13: AppColors.grey13,
+          primary6: AppColors.primary6,
+          primary11: AppColors.primary11,
+          redShade: AppColors.redShade,
+          popUpBg: AppColors.popUpBg,
+          orangeShade: AppColors.orangeShade,
+          barrierColor: AppColors.barrierColor,
+          iconBackground2: AppColors.iconBackground2,
+          iconBackground3: AppColors.iconBackground3,
+          iconBackground: AppColors.iconBackground,
+          white: AppColors.white,
         )
       ],
     );
